997f37e438e826eb1955fd16b95794ccb233e4df
Leandro Cesquini Pereira <leandro@leandrocp.com.br>
2018-10-19 15:53:36 -0300

    log compile stdout

--- lib/exsync/utils.ex
+++ lib/exsync/utils.ex
@@ -1,6 +1,10 @@
+require Logger
+
 defmodule ExSync.Utils do
   def recomplete do
-    System.cmd("mix", ["compile"], cd: ExSync.Config.app_source_dir())
+    "mix"
+    |> System.cmd(["compile"], cd: ExSync.Config.app_source_dir())
+    |> log_compile_cmd()
   end
 
   def unload(module) when is_atom(module) do
@@ -18,5 +22,12 @@ defmodule ExSync.Utils do
     {:ok, binary, _} = :erl_prim_loader.get_file(file)
     module = beam_path |> Path.basename(".beam") |> String.to_atom()
     :code.load_binary(module, file, binary)
+  end
+
+  defp log_compile_cmd({"", 0} = result), do: result
+
+  defp log_compile_cmd({stdout, _} = result) do
+    Logger.info(stdout)
+    result
   end
 end
